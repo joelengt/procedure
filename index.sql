6814fd1b0f790bdad23cbffa4d29fa075b0ccb71
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2018-07-02 22:51:55 -0500

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,41 @@
+CREATE PROCEDURE proc_sacar_clientes_tipo (IN tipoCliente INT)
+  BEGIN
+  SELECT * FROM clientes WHERE tipo = tipoCliente;
+  END
+
+DELIMITER //
+ CREATE PROCEDURE GetAllProducts()
+   BEGIN
+   SELECT *  FROM products;
+   END //
+ DELIMITER ;
+
+ 
+CALL proc_sacar_clientes_tipo(34);
+
+/* OFFSET => number od records to avoid */
+
+/* PAGINATION */
+SELECT id, name
+FROM product
+ORDER BY id ASC
+LIMIT 10
+OFFSET 0; /* PAGE 1 */
+
+SELECT id, name
+FROM product
+ORDER BY id ASC
+LIMIT 10
+OFFSET 10 /* PAGE 2 */
+
+SELECT id, name
+FROM product
+ORDER BY id ASC
+LIMIT 10
+OFFSET 20; /* PAGE 3 */
+
+SELECT id, name
+FROM product
+ORDER BY id ASC
+LIMIT 10
+OFFSET 30; /* PAGE 4 */
